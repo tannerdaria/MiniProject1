79c7657f4d48722ae946319adbe81e9a323267ab
Tanner Daria <89423851+tannerdaria@users.noreply.github.com>
2021-09-27 20:52:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+/*
+Mini Project 1
+Group 2: Tanner Daria, Alex Hils, Graham Koenig, Maggie Korte
+Date: 9/29
+*/
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to the Guessing Game!");
+
+    System.out.println("Please enter any positive whole number: ");
+  }
+}
