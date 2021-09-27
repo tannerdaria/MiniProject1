6772ca5932bf47ac0810daa3d709cfac202b341c
Tanner Daria <89423851+tannerdaria@users.noreply.github.com>
2021-09-27 21:32:40 +0000

    a little set up

--- Main.java
+++ Main.java
@@ -11,5 +11,25 @@ class Main {
     System.out.println("Welcome to the Guessing Game!");
 
     System.out.println("Please enter any positive whole number: ");
-  }
+
+    Scanner s = new Scanner(System.in);
+    Random r = new Random();
+
+    int userNum = s.nextInt();
+    int randomNum = r.nextInt(userNum);
+
+    int numbersForGame = playGame(randomNum, userNum);
+
+    System.out.println("A random number to guess has been generated.");
+
+  } 
+
+    public static int playGame(int guessNum, int originalNum){
+
+
+
+    System.out.println("Please guess a number between 0 and " + originalNum);
+      
+    }
+  
 }
