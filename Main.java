a585ba3ab4d81fa581869f430a89d8413bf38a70
Tanner Daria <89423851+tannerdaria@users.noreply.github.com>
2021-09-28 23:48:52 +0000

    completed game

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
-import java.util.*;
-
 /*
 Mini Project 1
-Group 2: Tanner Daria, Alex Hils, Graham Koenig, Maggie Korte
-Date: 9/29
+Group#2 Graham Koenig, Maggie Korte, Alex Hils, Tanner Daria
+9/29/21
 */
+
+import java.util.Scanner;
+import java.util.Random;
 
 class Main {
   public static void main(String[] args) {
     System.out.println("Welcome to the Guessing Game!");
-
     System.out.println("Please enter any positive whole number: ");
+
+    int guessNum;
+    int originalNum;
 
     Scanner s = new Scanner(System.in);
     Random r = new Random();
@@ -18,18 +21,38 @@ class Main {
     int userNum = s.nextInt();
     int randomNum = r.nextInt(userNum);
 
+    System.out.println("A random number to guess has been generarted.");
+
+    System.out.println("Please guess a number between 0 and " + userNum);
+
     int numbersForGame = playGame(randomNum, userNum);
+  }
 
-    System.out.println("A random number to guess has been generated.");
+  public static int playGame(int originalNum, int guessNum) {
 
-  } 
+    Scanner g = new Scanner(System.in);
+    int userGuess = g.nextInt();
+    int numTries = 1;
 
-    public static int playGame(int guessNum, int originalNum){
+    while (userGuess != originalNum){
 
-
-
-    System.out.println("Please guess a number between 0 and " + originalNum);
-      
+      if (userGuess > originalNum){ 
+          System.out.println("Guess lower");
+          System.out.println("Enter your new guess: "); 
+          userGuess = g.nextInt();
+            numTries++;
+          }
+       if (userGuess < originalNum) {
+          System.out.println("Guess higher");
+          System.out.println("Enter your new guess: ");
+          userGuess = g.nextInt();
+            numTries++;
+          }
     }
-  
-}
+      if (userGuess == originalNum){
+          System.out.println("Great, you win! It took you " + numTries + " tries");}
+      
+        return numTries;
+    }
+
+  }
